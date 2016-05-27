32608decf94c781a17dd26bbc07b48bac4ceb703
Ricardo Moran <Ricardo Moran>
2016-05-27 11:55:48 -0300

    Renamed Class1 to NLT and changed csproj and AssemblyInfo

--- src/NLT/NLT.cs
+++ src/NLT/NLT.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NLT
+namespace NaturalLanguageTranslator
 {
-    public class Class1
+    public class NLT
     {
     }
 }
--- src/NLT/Properties/AssemblyInfo.cs
+++ src/NLT/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLT")]
+[assembly: AssemblyTitle("RichoM.NLT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NLT")]
+[assembly: AssemblyCompany("Ricardo Moran")]
+[assembly: AssemblyProduct("RichoM.NLT")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
