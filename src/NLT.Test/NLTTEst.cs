10a733b83a871c6de59af02fc79edc4d11f0ac98
Ricardo Moran <Ricardo Moran>
2016-05-27 14:27:54 -0300

    Starting to add some tests.

---
+++ src/NLT.Test/NLTTEst.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NaturalLanguageTranslator;
+using System.IO;
+using System.Globalization;
+
+namespace NLTTest
+{
+    [TestClass]
+    public class NLTTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.GetCultureInfo("en");
+            NLT.Default = new NLT(new FileInfo("translations1.csv"));
+        }
+
+        [TestMethod]
+        public void CurrentLanguageIsConfiguredCorrectly()
+        {
+            Assert.AreEqual("en", NLT.Default.CurrentLanguage);
+        }
+    }
+}
