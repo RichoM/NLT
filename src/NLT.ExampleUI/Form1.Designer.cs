72d4048044a95361150a2f081ce2c25bff4dd768
Ricardo Moran <Ricardo Moran>
2016-05-28 23:58:54 -0300

    Added a very simple example GUI.

---
+++ src/NLT.ExampleUI/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace NLT_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.rememberCheckBox = new System.Windows.Forms.CheckBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Location = new System.Drawing.Point(12, 38);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(110, 23);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name:";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Location = new System.Drawing.Point(183, 117);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(112, 23);
+            this.acceptButton.TabIndex = 1;
+            this.acceptButton.Text = "Accept";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(128, 40);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(167, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // rememberCheckBox
+            // 
+            this.rememberCheckBox.AutoSize = true;
+            this.rememberCheckBox.Location = new System.Drawing.Point(128, 92);
+            this.rememberCheckBox.Name = "rememberCheckBox";
+            this.rememberCheckBox.Size = new System.Drawing.Size(125, 17);
+            this.rememberCheckBox.TabIndex = 3;
+            this.rememberCheckBox.Text = "Remember password";
+            this.rememberCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(183, 146);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(112, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancelar";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(128, 66);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(167, 20);
+            this.passwordTextBox.TabIndex = 7;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Location = new System.Drawing.Point(12, 64);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(110, 23);
+            this.passwordLabel.TabIndex = 6;
+            this.passwordLabel.Text = "Password:";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(330, 24);
+            this.menuStrip.TabIndex = 8;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // languageToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.languageToolStripMenuItem.Text = "Language";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 183);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(330, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripLabel
+            // 
+            this.toolStripLabel.Name = "toolStripLabel";
+            this.toolStripLabel.Size = new System.Drawing.Size(122, 17);
+            this.toolStripLabel.Text = "The current date is {0}";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 205);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.rememberCheckBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "ExampleForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button acceptButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.CheckBox rememberCheckBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripLabel;
+    }
+}
+
---
+++ src/NLT.ExampleUI/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NaturalLanguageTranslator;
+using System.Globalization;
+
+namespace NLT_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadAvailableLanguages();
+            UpdateUI();
+        }
+
+        private void LoadAvailableLanguages()
+        {
+            foreach (CultureInfo culture in NLT.Default.AvailableCultures)
+            {
+                ToolStripMenuItem menuItem = new ToolStripMenuItem(culture.DisplayName, null, (s, e) =>
+                {
+                    foreach (ToolStripMenuItem item in languageToolStripMenuItem.DropDownItems)
+                    {
+                        item.Checked = item == s;
+                    }
+                    NLT.Default.CurrentCulture = culture;
+                    UpdateUI();
+                });
+                menuItem.Checked = culture.Equals(NLT.Default.CurrentCulture);
+                languageToolStripMenuItem.DropDownItems.Add(menuItem);
+            }
+        }
+
+        private void UpdateUI()
+        {
+            Text = "This is an example".Translated();
+            languageToolStripMenuItem.Text = "Language".Translated();
+            nameLabel.Text = "Name:".Translated();
+            passwordLabel.Text = "Password:".Translated();
+            rememberCheckBox.Text = "Remember password".Translated();
+            acceptButton.Text = "Accept".Translated();
+            cancelButton.Text = "Cancel".Translated();
+            toolStripLabel.Text = string.Format("The current date is {0}".Translated(), DateTime.Now);
+        }
+    }
+}
