c4e15f5b0198ad9d746516b7b3a09bea9eaa9b7e
RichoM <richi.moran@gmail.com>
2016-05-30 16:39:06 -0300

    Added extension method ToEnumerable(..)
    It transforms a two-dimensional array or T to IEnumerable<IEnumerable<T>>.

--- src/NLT/Extensions.cs
+++ src/NLT/Extensions.cs
@@ -12,5 +12,20 @@ namespace NaturalLanguageTranslator
         {
             return NLT.Default.Translate(original);
         }
+
+        internal static IEnumerable<IEnumerable<T>> ToEnumerable<T>(this T[,] array)
+        {
+            List<List<T>> result = new List<List<T>>();
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                List<T> row = new List<T>();
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    row.Add(array[i, j]);
+                }
+                result.Add(row);
+            }
+            return result;
+        }
     }
 }
