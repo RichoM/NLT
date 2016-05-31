605fe4faf53aa73d2177225cd417a7b13b34e141
RichoM <richi.moran@gmail.com>
2016-05-30 22:03:35 -0300

    Removed ToEnumerable() extension method. Not we use the RichoM.CSV version

--- src/NLT/Extensions.cs
+++ src/NLT/Extensions.cs
@@ -12,20 +12,5 @@ namespace NaturalLanguageTranslator
         {
             return NLT.Default.Translate(original);
         }
-
-        internal static IEnumerable<IEnumerable<T>> ToEnumerable<T>(this T[,] array)
-        {
-            List<List<T>> result = new List<List<T>>();
-            for(int i = 0; i < array.GetLength(0); i++)
-            {
-                List<T> row = new List<T>();
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    row.Add(array[i, j]);
-                }
-                result.Add(row);
-            }
-            return result;
-        }
     }
 }
